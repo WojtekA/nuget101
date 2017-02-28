d0fc10cbdb407b989c1325e9d6230477399dcbd8
Wojciech Antonik <wojciech.antonik@pragmasoft.pl>
2017-02-28 23:26:32 +0100

    Use case project - init

---
+++ TrainingDay/WebFormApp/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                //EventLog.CreateEventSource("Application", "Application");
+                eventLog.Source = "Application";
+                eventLog.WriteEntry("Log message example", EventLogEntryType.Information);
+            }
+        }
+    }
+}
