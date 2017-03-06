abc7c5373d74c5279736b1a3a601c1ab19298c69
Wojciech Antonik <wojciech.antonik@pragmasoft.pl>
2017-03-06 22:46:20 +0100

    symbols package added

--- PackageSources/TrainingDay.Greeter/Greeter.cs
+++ PackageSources/TrainingDay.Greeter/Greeter.cs
@@ -1,10 +1,13 @@
-﻿namespace TrainingDay.Greeter
+﻿using TrainingDay.Greeter.test;
+
+namespace TrainingDay.Greeter
 {
     public static class Greeter
     {
         public static string GetGreeting(string name)
         {
-            return "Hello again! " + name;
+
+            return "Hello again! " + name + Class1.GetA();
         }
     }
 }
